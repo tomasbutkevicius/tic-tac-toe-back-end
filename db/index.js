ba7304bd19282f7e5cf023dd64b22ae0b96bb36c
Tomas <tomas.butkevicius99@gmail.com>
2021-04-21 16:39:01 +0300

    add local docker mongodb

--- db/index.js
+++ db/index.js
@@ -2,15 +2,19 @@ const mongoose = require("mongoose");
 
 function connect() {
     return new Promise((resolve, reject) => {
+        let databaseUrl = process.env.DB_CONNECTION;
+        if(process.env.DB_LOCAL === 'true'){
+            databaseUrl = "mongodb://mongo:27017/docker-node-mongo";
+        }
         if (process.env.NODE_ENV === 'test') {
             const Mockgoose = require('mockgoose').Mockgoose;
             const mockgoose = new Mockgoose(mongoose);
             mockgoose.prepareStorage()
                 .then(() => {
-                    mongooseConnect(process.env.DB_CONNECTION, resolve, reject);
+                    mongooseConnect(databaseUrl, resolve, reject);
                 });
         } else {
-            mongooseConnect(process.env.DB_CONNECTION, resolve, reject);
+            mongooseConnect(databaseUrl, resolve, reject);
         }
     });
 };
